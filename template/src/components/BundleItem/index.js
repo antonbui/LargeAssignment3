8d9cc30774ddb5ab6945754d7e19e4881e9bf2af
stefan <stefank17@ru.is>
2020-03-04 22:34:29 +0000

    total bundle price done

--- template/src/components/BundleItem/index.js
+++ template/src/components/BundleItem/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BundleItem = ({ id, name, items}) => (
+
+const BundleItem = ({ id, name, items, sum}) => (
     <div className="card card-product">
         <figcaption className="info-wrap">
             <h3><Link to={`/bundle/${id}`}>{ name }</Link></h3>
         </figcaption>
         <div className="bottom-wrap">
             <div className="price-wrap h5">
-                <span className="price">Total price: ${ items }</span>
+                <span className="price">Total price: ${ sum }</span>
             </div>
         </div>
     </div>
-  );
+);
   
   export default BundleItem;
   
--- template/src/components/BundleList/index.js
+++ template/src/components/BundleList/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import BundleItem from '../BundleItem';
 
-const BundleList = ({ bundles }) => (
+function sumPrice(items, bubbles) {
+    var sum = 0;
+    console.log(items);
+    let result = items.map(i => { 
+        return bubbles.map(b => { 
+            if (b.id==i) {
+                return b.price
+            }
+        })
+    })
+        
+    console.log("Result: " + result);
+    for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < result[i].length; j++) {
+            if (typeof result[i][j] != "undefined"){
+                sum = sum + result[i][j]
+            }
+        }
+    }
+    result = result.filter(word => word.length > 1);
+    return sum;
+};
+
+const BundleList = ({ bundles, bubbles }) => (
     <div className="bubble-list">
     { bundles.map(b => <BundleItem 
     key={ b.id }
     id={ b.id }
     name={ b.name }
     items={ b.items }
+    sum={ sumPrice(b.items, bubbles) }
+
     />)}
     </div>
 )
