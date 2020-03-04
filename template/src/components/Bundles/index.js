ad2cd88d9eab02997ad631d289ec25af1ea78898
stefan <stefank17@ru.is>
2020-03-04 17:38:32 +0000

    bundles working

--- template/src/components/Bundles/index.js
+++ template/src/components/Bundles/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import BundleList from '../BundleList';
+const Bundles = () => {
+    const [bundles, setData] = useState([]);
 
-const Bundles = () => (
+    function fetchData(){
+        const res = fetch("http://localhost:3500/api/bundles").then((res) => res.json()).then((res) => setData(res));
+    };
+
+    fetchData();
+    
+    return (
     <div>
         <h1>Bundles</h1>
         <p>Here comes the subpage for bundles</p>
+        {/* <h3>{bundles}</h3> */}
+        <BundleList
+        bundles={ bundles } />
     </div>
-);
+    );
+};
 
 export default Bundles;
